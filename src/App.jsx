56ad30c7e4bd0bddfdadb52acc94069315a64707
aleenadominic2003 <aleenadominic16@gmail.com>
2026-07-11 12:43:25 +0530

    Delete and navigation done

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddCar from './components/AddRegistration'
+import SearchCar from './components/SearchRegistration'
+import DeleteCar from './components/DeleteRegistration'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AddRegistration from './components/AddRegistration'
 import SearchRegistration from './components/SearchRegistration'
+import DeleteRegistration from './components/DeleteRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AddRegistration />
-      <SearchRegistration />
+      
+      <BrowserRouter>
+      
+        <Routes>
+
+          <Route path='/' element={<AddRegistration/>} />
+          <Route path='/search' element={<SearchRegistration/>} />
+          <Route path='/delete' element={<DeleteRegistration/>} />
+        
+
+        </Routes>
+
+      </BrowserRouter>
+
     </>
   )
 }
